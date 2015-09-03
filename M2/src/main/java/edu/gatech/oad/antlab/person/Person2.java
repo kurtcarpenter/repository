52293cb5c8f07fac84c6bf09624206b980e360d3
kurtcarpenter <kurtcarpenter@hotmail.com>
2015-09-03 12:25:29 -0400

    Person2 Completed
    String randomization

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,14 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.Random;
+import java.lang.StringBuilder;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
- * @version 1.1
+ * @author Kurt Carpenter
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -30,15 +32,23 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+    StringBuilder oldString = new StringBuilder(input);
+    StringBuilder newString = new StringBuilder();
+    Random rnd = new Random();
+    int index;
+    while (oldString.length() > 0) {
+      index = rnd.nextInt(oldString.length());
+      newString.append(oldString.charAt(index));
+      oldString.deleteCharAt(index);
+    }
+    return newString.toString();
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
